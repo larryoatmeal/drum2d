432142ecb4a8e69bb81ba9be698443e879631829
larryoatmeal <larryoatmeal@gmail.com>
2018-09-15 17:49:20 -0400

    setup drumboard

--- backend/debug.js
+++ backend/debug.js
@@ -1,5 +1,26 @@
-var socket = io.connect();
 
-socket.on('connect', function(data) {
-	socket.emit('join', 'Hello World from client');
-});
+window.onload = () => {
+	//debug graphing
+	var smoothie = new SmoothieChart();
+	smoothie.streamTo(document.getElementById("audiocanvas"));
+	var leftSeries = new TimeSeries();
+	var rightSeries = new TimeSeries();
+	smoothie.addTimeSeries(leftSeries, { strokeStyle:'rgb(255, 0, 0)', fillStyle:'rgba(0, 0, 0, 0)', lineWidth:3 });
+	smoothie.addTimeSeries(rightSeries,{ strokeStyle:'rgb(0, 255, 0)', fillStyle:'rgba(0, 0, 0, 0)', lineWidth:3 });
+
+
+	var socket = io.connect();
+
+	socket.on('connect', function(data) {
+		socket.emit('join', 'Hello World from client');
+
+		socket.on('rawAudio', (data)=>{
+			console.log(data);
+			leftSeries.append(new Date().getTime(), data[0]);
+			rightSeries.append(new Date().getTime(), data[1])
+
+		})
+	});
+
+}
+
--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,7 @@ const op = require('rxjs/operators')
 const express = require('express');
 const Readline = require('@serialport/parser-readline')
 
-
-
-
-
 var SerialPort = require('serialport');
-
 const app = express();
 // var server = require('http').createServer(app);  
 
@@ -63,26 +58,49 @@ function setupSerial(serialPort){
 	// });	
 }
 
-$serialPort.pipe(
+$rawAudio = $serialPort.pipe(
 	op.skip(5), //skip first few
-	op.throttleTime(1000),
 	// op.tap(console.log),
 	op.map(str =>  {
 		split = str.split('\t');
+		// console.log("SPLIT")
+		// console.log(split)
 		left = split[0]
-		right = split[0]
-		left = left.substring(4, left.length);//number starts on 4th character
-		right = right.substring(4, right.length);
+		right = split[1]
+		// left = left.substring(4, left.length);//number starts on 4th character
+		// right = right.substring(4, right.length);
 		return [parseInt(left), parseInt(right)]
-	})
+	}),
 )
 
-$serialPort.subscribe(console.log);
-
-// $output = $serialPort;
 
 
-$output = rx.interval(1000);
+
+
+$output = $rawAudio.pipe(
+	// op.throttleTime(1),
+	op.map(data => ({
+		"key": "rawAudio",
+		"value": data
+	}))
+);
+
+// $serialPort.subscribe(console.log)
+// $output = rx.interval(1000).pipe(
+// 	op.map(i => ({
+// 		"key": "rawAudio",
+// 		"value": [i, i]
+// 	}))
+// );
+
+// $output = $serialPort
+
+// .pipe(
+// 	op.map(data => ({
+// 		"key": "rawAudio",
+// 		"value": data
+// 	}))
+// );
 
 var clients = {};
 
@@ -90,6 +108,8 @@ $status = rx.interval(5000).pipe().subscribe(() => {
 
 	console.log("CLIENTS", Object.keys(clients));
 });
+
+
 
 
 io.on('connection', function(client) {  
@@ -106,7 +126,7 @@ io.on('connection', function(client) {
 	})
 
 	client.on('join', function(data) {
-    	console.log(data);
+    	// console.log(data);
         client.emit('messages', 'Hello from server, streaming data now');
 
         $output.pipe(op.takeUntil($unsubscribe)).subscribe(data => {
