e2b39fde014b40a250fce680d2efd47e50970735
Irene Zhou <zhoui@MIT.EDU>
2018-09-15 18:37:29 -0400

    updated drumpad UI

--- frontend/init.js
+++ frontend/init.js
@@ -3,40 +3,16 @@ window.onload = main;
 var socket = io.connect("http://fffe2f02.ngrok.io/");
 
 function main() {
-	
-
-	socket.on('connect', function(data) {
-		socket.emit('join', 'Hello World from client');
-	});
-
-	socket.on('messages', function(data) {
-                alert(data);
-        });
-
-	socket.on(/*this is the event for which pad to light up*/'padnum', function(data) {
-		lightUpDrumPad(data);
-		setTimeout(function(){darkenDrumPad(data)}, 200);
-	});
-
-	
-	renderDrumPad(4);
-	lightUpDrumPad(1);
-	lightUpDrumPad(2);
-	setTimeout(function(){darkenDrumPad(1)}, 200);
-	setTimeout(function(){changeNumPads(3)}, 1000);
-
+	renderDrumPad(2);
+	lightUpDrumPad(0, 0.25);
+	lightUpDrumPad(1, 0.5);
+	setTimeout(function(){darkenDrumPad(1)}, 1000);
 }
 
+/*
 function updateNumPads(numPads) {
 	changeNumPads(numPads);
 	socket.emit('updateNumPads', numPads);
 }
-
-
-function startCalibration() {
-
-}
-
-
-function calibrate
+*/
 
