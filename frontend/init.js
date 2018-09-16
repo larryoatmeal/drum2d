fe826a5bed9b43b4782774176e1c2e31cea39735
Irene Zhou <zhoui@MIT.EDU>
2018-09-15 23:09:05 -0400

    sound to pads

--- frontend/init.js
+++ frontend/init.js
@@ -14,18 +14,30 @@ function main() {
 	onHit(hit);
 }
 
+var cur_bank = '808';
 
 function hit(n){
 	if (n.hand == "left"){
+		playSound(cur_bank, 1, n.strength);
 		lightUpDrumPad(0,n.strength);
 		setTimeout(function(){darkenDrumPad(0)}, 100);
 	}
 
 	else{
+		playSound(cur_bank, 1, n.strength);
 		lightUpDrumPad(1,n.strength);
 		setTimeout(function(){darkenDrumPad(1)}, 100);
 	}
+	console.log(n);
 }
+
+
+function loadAndPlay(bankname){
+	loadDrumBank(bankname, console.log);
+	cur_bank = bankname;
+}
+
+
 
 /*
 function updateNumPads(numPads) {
