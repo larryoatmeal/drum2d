ae36fc71a0c9d162cbf7d5f28cb87ac1a5cb0bbd
Emily Hu <emilyhu@mit.edu>
2018-09-15 20:34:45 -0400

    Implement scratch detect

--- frontend/scratch.js
+++ frontend/scratch.js
@@ -1,5 +1,38 @@
-function detectScratch() {
-	console.log(event.pageX + ", " + event.pageY);
-}
+let scratching = false;
 
 $(document).mousemove(detectScratch);
+
+function detectScratch(event) {
+	let coords = [];
+
+	$(document).mousemove(addCoords)
+
+	function addCoords(e) {
+		coords.push({x: e.pageX, y: e.pageY})
+	}
+
+	setTimeout(() => checkMotion(coords), 100)
+}
+
+function checkMotion(coords) {
+	let all_x = coords.map((coord) => coord.x);
+	let all_y = coords.map((coord) => coord.y);
+
+	let x_diff = Math.max(...all_x) - Math.min(...all_x);
+	let y_diff = Math.max(...all_y) - Math.min(...all_y);
+
+	if (y_diff > 150 && x_diff < 100) {
+		if (!scratching) {
+			scratching = true;
+			createScratch();
+		}
+	}
+}
+
+function createScratch() {
+	setTimeout(() => {
+		console.log("scratch scratch!");
+		scratching = false
+	}, 5000);
+}
+
